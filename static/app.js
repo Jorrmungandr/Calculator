654eb73c7984b1407499e0215ae3a5ec2e8e8212
Yorrmungand <edgarmarques@hotmail.com.br>
2019-04-19 10:58:04 -0300

    Added Comments

--- static/app.js
+++ static/app.js
@@ -1,23 +1,30 @@
+//The main html elements
 const calc = document.querySelector('#calculator');
 const functions = document.querySelector('#main-functions');
 const textContainer = document.querySelector('#text-container');
 const enterButton = document.querySelector('#enter');
 const cleanButton = document.querySelector('#C');
 
+//The digits and main functions
 let digits = ['C', '()', '%', '1', '2', '3', '4', '5', '6', '7', '8', '9', '±', '0', '.'];
 let mainFunctions = ['/', '*', '-', '+'];
+//The factor that determinates if or not to "breakline"
 let i = 0;
+//The "breakline"
 let float = '';
-let operation;
 
+//  When the Enter Button is pressed, the math that is in the input is evaluated and 
+//the input value turns into the result
 const handleEnter = () => {
     textContainer.value = eval(textContainer.value);
 }
 
+//Cleans the input value
 const handleClean = () => {
     textContainer.value = '';
 }
 
+//Making NumPad
 digits.forEach(element => {
     if ((i - 2) % 3 === 0) {
         float = '';
@@ -36,13 +43,13 @@ digits.forEach(element => {
     i++;
 });
 
+//Making Functions Pad
 mainFunctions.forEach(element => {
     let btn = document.createElement('Button');
     btn.className = 'btn btn-primary btn-block';
     btn.id = element;
     btn.onclick = () => {
         textContainer.value += element;
-        operation = element;
     };
     btn.style.width = '50px';
     btn.style.height = '50px';
@@ -53,11 +60,13 @@ mainFunctions.forEach(element => {
     }
     functions.appendChild(btn);
 });
-
+//Color of the buttons
 document.querySelector('#C').style.webkitTextFillColor = 'red';
+enterButton.style.webkitTextFillColor = 'blue';
+
+//Event Listeners
 document.querySelector('#C').onclick = () => {
     handleClean();
 }
 
 enterButton.addEventListener('click', handleEnter);
-enterButton.style.webkitTextFillColor = 'blue';
